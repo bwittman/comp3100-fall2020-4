7174f6be5fc31ad0d7fe806f018355084a2e1800
Connor May <poiu2463@gmail.com>
2020-10-17 10:46:12 -0400

    Added rules popup code
    added code to try and open a .txt with the rules in it

--- src/battleship/MainWindow.java
+++ src/battleship/MainWindow.java
@@ -1,7 +1,12 @@
 package battleship;
 
 import javax.swing.*;
+
+
+
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class MainWindow extends JPanel {
     private ImageIcon background = new ImageIcon("src\\pictures\\mainWindow.png");
@@ -23,11 +28,25 @@ public class MainWindow extends JPanel {
         JButton rulesButton = new JButton("Rules");
         JButton networkButton = new JButton("Networking");
         JButton onePlayerButton = new JButton("Play Against Computer");
-
+        
+        rulesButton.addActionListener(e->{
+        	Desktop myDesktop = Desktop.getDesktop();
+        	if(Desktop.isDesktopSupported()) {
+        		try {
+                	File rules = new File("src\\pictures\\rules.txt");
+                	myDesktop.open(rules);
+        		}catch(IOException ex){
+        			//TODO: warning window No application registered for opening .txt files
+        			System.err.println("No application registered for opening .txt files");
+        		}
+        	}
+        });
+        
         panel.add(onePlayerButton);
         panel.add(rulesButton);
         panel.add(networkButton);
-
+        
+        
         this.add(panel, BorderLayout.SOUTH);
     }
 }
