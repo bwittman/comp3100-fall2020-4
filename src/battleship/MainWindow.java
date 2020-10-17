1434868757f16cdb90314f77cc19c3d6c01c7827
Connor May <poiu2463@gmail.com>
2020-10-17 12:18:14 -0400

    Fixed issue with networking popup
    Fixed issue with networking popup where if you closed the popup without selecting the program would think that you had selected join

--- src/battleship/MainWindow.java
+++ src/battleship/MainWindow.java
@@ -32,7 +32,7 @@ public class MainWindow extends JPanel {
         networkButton.addActionListener(e->{
         	Object[] options = {"Host", "Join"};
         	int response = JOptionPane.showOptionDialog(null, "Will you Host or Join?", "Host or Join?", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
-        	
+        	System.out.println("Player responded: " + response);
         	if(response == 0) { //User Selected Host
         		System.out.println("User Selected Host");
         		JPanel hostPanel = new JPanel(new GridLayout(4,1));
@@ -42,7 +42,7 @@ public class MainWindow extends JPanel {
         		hostPanel.add(localIP);
         		this.add(hostPanel, BorderLayout.CENTER);	 //TODO: asked dr. wittman about this and how to draw over the current JPanel
         		
-        	}else {				//User Selected Join 
+        	}else if(response == 1) {				//User Selected Join 
         		System.out.println("User Selected Join");
         	}
         });
