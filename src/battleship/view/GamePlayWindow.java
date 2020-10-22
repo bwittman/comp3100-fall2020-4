af6266b2f2e01be8dbf8d7a7ef46fbd80c222355
dosch1 <dosch1@otterbein.edu>
2020-10-22 11:58:10 -0400

    stubbed out methods for GamePlayWindow

--- src/battleship/view/GamePlayWindow.java
+++ src/battleship/view/GamePlayWindow.java
@@ -4,12 +4,19 @@ import javax.swing.*;
 
 public class GamePlayWindow extends JFrame {
 
-    Board userBoard;
-    Board enemyBoard;
+    private Board userBoard;
+    private Board enemyBoard;
+    private JPanel log;
+    private JPanel key;
+    private JPanel options;
 
     public GamePlayWindow(){
         userBoard = new Board();
         enemyBoard = new Board();
+
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE); //might change later
+
+
     }
 
     public Board getUserBoard() {
@@ -19,5 +26,21 @@ public class GamePlayWindow extends JFrame {
     public Board getEnemyBoard() {
         return enemyBoard;
     }
+
+    public void writeToLog(){
+    }
+
+    public void placeShips(){
+    }
+
+    private void selectStart(){
+    }
+
+    private void selectEnd(){
+    }
+
+    private void placeAShip(){
+    }
+
 
 }
