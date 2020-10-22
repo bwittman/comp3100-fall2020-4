a16410482423b39c6cf791245e778b8bb34db3e8
Connor May <poiu2463@gmail.com>
2020-10-22 13:11:38 -0400

    Added Networking Client View

---
+++ src/battleship/view/NetworkingClient.java
@@ -0,0 +1,55 @@
+package battleship.view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GraphicsConfiguration;
+import java.awt.GridBagLayout;
+import java.awt.HeadlessException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class NetworkingClient extends JFrame {
+	
+	JTextField IPinput;
+	JButton connectButton;
+	
+	
+	public NetworkingClient() {
+		setTitle("Battleship - Host");
+		setSize(400,200);
+        setMinimumSize(new Dimension(400,200));
+		setResizable(false);
+		
+		JPanel outerPanel = new JPanel(new BorderLayout());
+		JPanel topPanel = new JPanel(new GridBagLayout());
+		JPanel middlePanel = new JPanel(new GridBagLayout());
+		JPanel bottomPanel = new JPanel(new GridBagLayout());
+		
+		JLabel directions = new JLabel("Enter the host's IP address: ");
+		IPinput = new JTextField(11);
+		connectButton = new JButton("Connect!");
+		
+		topPanel.add(directions);
+		middlePanel.add(IPinput);
+		bottomPanel.add(connectButton);
+		outerPanel.add(topPanel, BorderLayout.NORTH);
+		outerPanel.add(middlePanel, BorderLayout.CENTER);
+		outerPanel.add(bottomPanel, BorderLayout.SOUTH);
+		
+		add(outerPanel);
+		pack();
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		NetworkingClient test = new NetworkingClient();
+
+	}
+
+}
