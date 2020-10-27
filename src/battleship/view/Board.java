7f91b6d39cc9898dcc40f0d8abdde12adec7e886
Dominic Ravagnani <dominicravman@gmail.com>
2020-10-27 12:11:10 -0400

    Started working on GamePlayWindow. It isn't formatted correctly.

--- src/battleship/view/Board.java
+++ src/battleship/view/Board.java
@@ -9,13 +9,14 @@ public class Board extends JPanel {
 
     private static final int ROWS = 10;
     private static final int COLUMNS = 10;
-    private static final int BUTTON_SIDE = 50;
+    private static final int BUTTON_SIZE = 30;
 
     private JButton[][] buttonArray = new JButton[ROWS][COLUMNS];
     private Player player;
 
     public Board() {
-        this.setLayout(new GridLayout(ROWS,COLUMNS));
+        this.setLayout(new GridLayout(ROWS + 1,COLUMNS + 1));
+        setUpLabels();
         setUpButtons();
     }
 
@@ -23,11 +24,22 @@ public class Board extends JPanel {
         return buttonArray[i][j];
     }
 
+    private void setUpLabels(){
+        for (int i = 0; i < COLUMNS; ++i){
+            JLabel label = new JLabel();
+            label.setText(Character.toString((char) i + (int) 'A'));
+            this.add(label);
+        }
+    }
+
     private void setUpButtons(){
         for (int i = 0; i < ROWS; i++ ) {
+            JLabel label = new JLabel();
+            label.setText(Integer.toString(i + 1));
+            this.add(label);
             for (int j = 0; j < COLUMNS; j++) {
                 JButton button = new JButton();
-                button.setPreferredSize(new Dimension(BUTTON_SIDE, BUTTON_SIDE));
+                button.setPreferredSize(new Dimension(BUTTON_SIZE, BUTTON_SIZE));
                 buttonArray[i][j] = button;
                 this.add(button);
             }
