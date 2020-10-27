e0c162163e1b77b9c96bca881a751be20853e33c
Connor May <poiu2463@gmail.com>
2020-10-27 12:25:31 -0400

    Implemented Connection
    Needs Testing

--- src/battleship/controller/Networking.java
+++ src/battleship/controller/Networking.java
@@ -1,11 +1,17 @@
 package battleship.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Enumeration;
+import java.util.Scanner;
 
 public class Networking {
 	
@@ -13,9 +19,16 @@ public class Networking {
 	private ServerSocket serverSocket;
 	private Socket socket;
 	private static final int PORT = 7777;
+	private boolean isHost;
+	Scanner input;
+	OutputStream output;
+	
+	
 	
 	public Networking() {
 		String ip;
+		serverSocket = null;
+		socket = null;
 	    try {
 	        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
 	        while (interfaces.hasMoreElements()) {
@@ -34,22 +47,101 @@ public class Networking {
 	    } catch (SocketException e) {
 	    	hostIpAddress = "0.0.0.0";
 	    }
+	    
 	}
 	
-	public String getIPAddress() {
+	/**
+	 * Attempts to connect client to host
+	 * @param isHost tells class if it is the host or client
+	 * @param ipAddress if the class is not a host it needs to know the IP it should connect to
+	 * @return returns true if connected and false if not connects
+	 */
+	public boolean connect(boolean isHost, String ipAddress) {
+		
+		if(isHost) {
+	    	try {
+				serverSocket = new ServerSocket(PORT);
+				socket = serverSocket.accept();
+				InputStream inputStream = socket.getInputStream();
+				input = new Scanner(inputStream);
+				output = socket.getOutputStream();
+				
+			} catch (IOException e) {
+				System.out.println("IOException thrown in Networking Class Constructor (isHost = true)");
+				e.printStackTrace();
+				return false;
+			}
+	    }else {
+	    	try {
+				socket = new Socket(ipAddress, PORT);
+				
+			} catch (UnknownHostException e) {
+				System.out.println("UnknownHostException thrown in Networking Class Constructor");
+				e.printStackTrace();
+				return false;
+			} catch (IOException e) {
+				System.out.println("IOException thrown in Networking Class Constructor (isHost = false)");
+				e.printStackTrace();
+				return false;
+			}
+	    }
+		
+		return true;
+	}
+	
+	/**
+	 * Getter for the IPAddress
+	 * @return a string representation of the IPAddress
+	 */
+	public String getIP() {
 		//TODO: Needs test case to check IP Address
 		return hostIpAddress;
 	}
 	
+	/**
+	 * Tries to send a message over the socket
+	 * @param message
+	 */
 	public void sendMessage(String message) {
-		//TODO: implement
+		if(socket != null && socket.isConnected()) {
+			try {
+				output.write(message.getBytes());
+			} catch (IOException e) {
+				System.out.println("Could not send Message IOException thrown");
+				e.printStackTrace();
+			};
+		}else {
+			System.out.println("Could not send message. Socket could be null or not connected");
+		}
 	}
 	
+	/**
+	 * Tries to get a message from the socket
+	 * @return returns the string from the socket if possible. Otherwise returns an empty string.
+	 */
 	public String recieveMessage() {
-		//TODO: implement
-		return "";
+		if(socket != null && socket.isConnected()) {
+			return input.nextLine();
+		}else {
+			return "";
+		}
 	}
 	
+	/**
+	 * Closes all input and output streams and sockets.
+	 */
+	public void cleanUp() {
+		try {
+			output.close();
+			input.close();
+			socket.close();
+		} catch (IOException e) {
+			System.out.println("Could not clean up socket IOException thrown");
+			e.printStackTrace();
+		}
+	}
 	
-	
+	public boolean isConnected() {
+		return socket.isConnected();
+	}
 }
