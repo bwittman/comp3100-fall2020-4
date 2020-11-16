93f357104e42ca3759d8cea1d1bb0256d0a82005
Connor May <poiu2463@gmail.com>
2020-11-16 11:56:57 -0500

    Fixed some merge issues

--- src/battleship/controller/HumanPlayer.java
+++ src/battleship/controller/HumanPlayer.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.NoSuchElementException;
 import javax.swing.*;
 
 import battleship.model.Results;
